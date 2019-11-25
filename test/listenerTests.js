66e3115c6d4f323026e302f3dd197583f9566bd0
Simon Strom <simon.strom@gmail.com>
2019-11-25 13:14:30 +0100

    Fix flaky tests
    Separate topics between tests

--- test/listenerTests.js
+++ test/listenerTests.js
@@ -6,7 +6,7 @@ const assert = require("assert");
 // Note requires running kafka instance on localhost:9092
 
 describe("exp-kafka-listener", () => {
-  let topic1, topic2;
+  let topic1, topic2, topic3;
   before(helper.init);
   before((done) => {
     helper.createTopic((err, name) => {
@@ -20,6 +20,13 @@ describe("exp-kafka-listener", () => {
       done(err);
     });
   });
+  before((done) => {
+    helper.createTopic((err, name) => {
+      topic3 = name;
+      done(err);
+    });
+  });
+
   describe("#listen to one topic", () => {
     const group = `test-group-${helper.randomString(5)}`;
     let listener;
@@ -42,6 +49,7 @@ describe("exp-kafka-listener", () => {
       msgs.forEach((m) => helper.send(topic1, m));
       listener.readStream.pipe(msgHandler);
     });
+
     it("should have client id named after package name / NODE_ENV", () => {
       assert.equal(listener.readStream.consumer.globalConfig['client.id'], "exp-kafka-listener-exp-kafka-listener-development")
     })
@@ -49,9 +57,10 @@ describe("exp-kafka-listener", () => {
 
   describe("#listen to multiple topics", () => {
     const group = `test-group-${helper.randomString(5)}`;
-    let listener;
+    let listener = xprKafkaListener.listen({ host: "localhost:9092" }, group, [topic2, topic3]);
+
     it("should emit 'ready' event once connected to kakfa", (done) => {
-      listener = xprKafkaListener.listen({ host: "localhost:9092" }, group, [topic1, topic2]);
+      listener = xprKafkaListener.listen({ host: "localhost:9092" }, group, [topic2, topic3]);
       listener.on("ready", done);
     });
 
@@ -67,7 +76,7 @@ describe("exp-kafka-listener", () => {
       });
       const msgs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(String);
       msgs.forEach((m, i) => {
-        const topic = (i % 2 === 0) ? topic1 : topic2;
+        const topic = (i % 2 === 0) ? topic2 : topic3;
         helper.send(topic, m);
       });
       listener.readStream.pipe(msgHandler);
